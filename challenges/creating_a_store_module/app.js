c0a8400d63caf445ca707b79b21354256655d0b3
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-04 17:52:33 -0400

    Challenge: Creating A Store Module

---
+++ challenges/creating_a_store_module/app.js
@@ -0,0 +1,5 @@
+// 1. Create a Module named gemStore. 
+// 2. Attach the gemStore module to our HTML page with a Directive. 
+// 3. In index.html, create a simple Expression to display a friendly "Hello Angular!" message.
+
+var app = angular.module("gemStore", [ ]);
